7a41dbd4e5093d5c7c27f647816228a14dbaa4f4
aneeslml <aneeslml@gmail.com>
2024-02-28 21:15:12 +0000

    feat: add skeleton component

---
+++ src/components/SkeletonPathway.tsx
@@ -0,0 +1,16 @@
+const SkeletonPathway = () => {
+  return (
+    <div className="pathway-card loading">
+      <div className="skeleton-image"></div>
+      <div className="pathway-card__content">
+        <div className="skeleton-info"></div>
+        <div className="skeleton-title"></div>
+        <div className="skeleton-text"></div>
+        <div className="skeleton-text"></div>
+        <div className="skeleton-button"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonPathway;

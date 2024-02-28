d6fc8cd4f88701bcebbca8eae57bffc722b57340
aneeslml <aneeslml@gmail.com>
2024-02-28 19:05:43 +0000

    fix: remove search from header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -6,13 +6,6 @@ const Header = () => {
         <img src="logo.svg" alt="Logo" className="logo-img" />
         <span className="logo-text">Library</span>
       </div>
-      <div className="search-section">
-        <input
-          type="text"
-          placeholder="Search for books"
-          className="search-input"
-        />
-      </div>
     </header>
   );
 };

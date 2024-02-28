a76f3ba1d90a64ce1cfb32e4cfdf6f32adaf5de8
aneeslml <aneeslml@gmail.com>
2024-02-28 16:43:17 +0000

    feat: pathway interface

---
+++ src/interfaces/pathway.ts
@@ -0,0 +1,11 @@
+export interface IPathway {
+  id: number;
+  title: string;
+  internal_title: string;
+  url: string;
+  intro: string;
+  duration: string;
+  image: string;
+  type: string;
+  has_summative_assessment: boolean;
+}

93ba9df971d9b412a8a527e19c1be9771a0a35d6
aneeslml <aneeslml@gmail.com>
2024-02-28 21:54:24 +0000

    feat: responsive pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,17 +1,79 @@
 import "./Pagination.css";
 
-const Pagination = ({ totalPages, currentPage, handlePageChange }: any) => {
+interface IPaginationProps {
+  totalPages: number;
+  currentPage: number;
+  handlePageChange: (page: number) => void;
+}
+
+const Pagination = ({
+  totalPages,
+  currentPage,
+  handlePageChange,
+}: IPaginationProps) => {
+  const renderPaginationNumbers = () => {
+    const paginationNumbers = [];
+
+    let start = Math.max(1, currentPage - 2);
+    let end = Math.min(start + 4, totalPages);
+
+    if (end - start < 4) {
+      start = Math.max(1, end - 4);
+    }
+
+    for (let i = start; i <= end; i++) {
+      paginationNumbers.push(
+        <button
+          key={i}
+          onClick={() => handlePageChange(i)}
+          className={currentPage === i ? "active" : ""}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    if (start > 1) {
+      if (start > 2) {
+        paginationNumbers.unshift(<span key="ellipsis1">...</span>);
+      }
+      paginationNumbers.unshift(
+        <button key="first" onClick={() => handlePageChange(1)}>
+          1
+        </button>
+      );
+    }
+
+    if (end < totalPages) {
+      if (end < totalPages - 1) {
+        paginationNumbers.push(<span key="ellipsis2">...</span>);
+      }
+
+      paginationNumbers.push(
+        <button key={totalPages} onClick={() => handlePageChange(totalPages)}>
+          {totalPages}
+        </button>
+      );
+    }
+
+    return paginationNumbers;
+  };
+
   return (
     <div className="pagination">
-      {[...Array(totalPages).keys()].map((page) => (
-        <button
-          key={page + 1}
-          onClick={() => handlePageChange(page + 1)}
-          className={currentPage === page + 1 ? "active" : ""}
-        >
-          {page + 1}
-        </button>
-      ))}
+      <button
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Prev
+      </button>
+      {renderPaginationNumbers()}
+      <button
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
     </div>
   );
 };

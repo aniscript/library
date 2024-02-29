9be974b6d252e2df075099fc9e0413de53025707
aneeslml <aneeslml@gmail.com>
2024-02-29 23:18:39 +0000

    chore: change not found message to pathways component

--- src/components/Pathways/Pathways.tsx
+++ src/components/Pathways/Pathways.tsx
@@ -4,6 +4,9 @@ import Pathway from "../Pathway/Pathway";
 import "./Pathways.css";
 
 const Pathways = ({ pathways }: { pathways: IPathway[] }) => {
+  if (pathways.length === 0)
+    return <p className="message">No pathways found.</p>;
+
   return (
     <section className="pathways">
       {pathways.map((pathway) => (
